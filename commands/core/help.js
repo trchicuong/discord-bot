fcdad82d3fb00ec5c628606ebc6de079c3cbc61e
Trần Chí Cường <pokekeeze1@gmail.com>
2025-02-13 22:07:13 +0700

    Update link contact server

--- commands/core/help.js
+++ commands/core/help.js
@@ -12,7 +12,7 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor('#5cb85c')
             .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true }) })
-            .setDescription(await Translate('The application Bot developed by cuongisreal IT.<\n>This project is open source, can be found <[here](https://github.com/trchicuong/discord-bot)>.<\n>If you have difficulty, need support join the Discord support server <[here](https://discord.gg/Pvw2vywSwv)>.'))
+            .setDescription(await Translate('The application Bot developed by cuongisreal IT.<\n>This project is open source, can be found <[here](https://github.com/trchicuong/discord-bot)>.<\n>If you have difficulty, need support join the Discord support server <[here](https://discord.gg/NNPDFufe4w)>.'))
             .addFields([{ name: `Enabled - ${commands.size}`, value: commands.map(x => `\`${x.name}\``).join(' | ') }])
             .setTimestamp()
             .setFooter({ text: await Translate('Developed with love by cuongisreal IT <❤️>'), iconURL: inter.member.displayAvatarURL({ dynamic: true }) });
